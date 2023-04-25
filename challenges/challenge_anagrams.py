e5c5d30f92e2c4526da4fe811ec6be15cf338142
DannOut <danielxdn@gmail.com>
2023-04-25 14:22:07 -0300

    feat(req 4 done): req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,60 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_list = list(first_string.lower())
+    second_list = list(second_string.lower())
+
+    quickSort(first_list)
+    quickSort(second_list)
+
+    if first_string == "" or second_string == "":
+        anagram = False
+    else:
+        anagram = first_list == second_list
+
+    return (stringfication(first_list), stringfication(second_list), anagram)
+
+
+# https://panda.ime.usp.br/panda/static/pythonds_pt/05-OrdenacaoBusca/OQuickSort.html
+def quickSort(alist):
+    quickSortHelper(alist, 0, len(alist) - 1)
+
+
+def quickSortHelper(alist, first, last):
+    if first < last:
+
+        splitpoint = partition(alist, first, last)
+
+        quickSortHelper(alist, first, splitpoint - 1)
+        quickSortHelper(alist, splitpoint + 1, last)
+
+
+def partition(alist, first, last):
+    pivotvalue = alist[first]
+
+    leftmark = first + 1
+    rightmark = last
+
+    done = False
+    while not done:
+
+        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+
+        while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark - 1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+
+    return rightmark
+
+
+def stringfication(list):
+    return "".join(list)
