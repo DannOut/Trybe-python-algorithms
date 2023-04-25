f9e24ccc6cd16c3f35af90f43517a8485dbfb91c
DannOut <danielxdn@gmail.com>
2023-04-25 13:55:39 -0300

    feat(refact req 1, done req 2): req 1 e 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    students_counter = 0
-
-    if not target_time or not permanence_period:
+    try:
+        students_counter = 0
+        for class_starting, class_finished in permanence_period:
+            if class_starting <= target_time <= class_finished:
+                students_counter += 1
+        return students_counter
+    except TypeError:
         return None
-
-    for class_starting, class_finished in permanence_period:
-        if (type(class_starting) is not int) or type(
-            class_finished
-        ) is not int:
-            return None
-        if class_starting <= target_time <= class_finished:
-            students_counter += 1
-
-    return students_counter
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    test_to_crypto = "daniel"
+
+    # teste em número impar
+    assert encrypt_message(test_to_crypto, 4) == "le_inad"
+
+    # teste em número impar
+    assert encrypt_message(test_to_crypto, 5) == "einad_l"
+
+    # teste em length maior que o test_to_crypto
+    assert encrypt_message(test_to_crypto, 10) == "leinad"
+
+    # teste caso parãmetro seja errado
+    with pytest.raises(TypeError):
+        encrypt_message(3, test_to_crypto)  # type: ignore
