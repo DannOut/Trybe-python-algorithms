05017cd541e144b566d1c9487e2b2697a4306a80
DannOut <danielxdn@gmail.com>
2023-04-25 14:43:30 -0300

    feat(req 5 done): req 5 done

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+
+    counter = 1
+    number = False
+
+    for i in range(1, len(nums)):
+        curr_num = nums[i]
+
+        if type(curr_num) != int or curr_num < 1:
+            return False
+
+        if curr_num == nums[i - 1]:
+            number = curr_num
+            counter += 1
+
+    return number
