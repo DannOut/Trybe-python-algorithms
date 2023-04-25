f119306fae70fbf1353a28e18c7625eed21269c7
DannOut <danielxdn@gmail.com>
2023-04-25 13:41:52 -0300

    feat(req 1 done): req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    students_counter = 0
+
+    if not target_time or not permanence_period:
+        return None
+
+    for class_starting, class_finished in permanence_period:
+        if (type(class_starting) is not int) or type(
+            class_finished
+        ) is not int:
+            return None
+        if class_starting <= target_time <= class_finished:
+            students_counter += 1
+
+    return students_counter
